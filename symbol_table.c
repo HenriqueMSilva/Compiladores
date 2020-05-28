3cfbe5d47ba0c9e426d7653fdab5f134c79803ef
Pedro Almeida <pedrodavidsa@gmail.com>
2020-05-28 19:19:24 +0100

    Xor && Main verification

--- symbol_table.c
+++ symbol_table.c
@@ -404,7 +404,7 @@ char * recursao_expr(is_expression_list* expr, method_var* lista_do_metodo ){
 				str2 = "stringarray";
 			}else if(strcmp(expr->expr2->tipo,"String[]") == 0 ){ 
 				str2 = "int";
-			}else if( strcmp(expr->expr1->operation,"Length") == 0 || strcmp(expr->expr1->operation,"ParseArgs") == 0 ){
+			}else if( strcmp(expr->expr2->operation,"Length") == 0 || strcmp(expr->expr2->operation,"ParseArgs") == 0 ){
                 str2 = "int";
             }else{
 				str2 = expr->expr2->tipo;
@@ -417,7 +417,6 @@ char * recursao_expr(is_expression_list* expr, method_var* lista_do_metodo ){
 
 		if((strcmp(expr->value,"Add") == 0 || strcmp(expr->value,"Sub") == 0 || strcmp(expr->value,"Mul") == 0 || strcmp(expr->value,"Div") == 0 || strcmp(expr->value,"Mod") == 0)){
 
-
 			if(strcmp(str1,"bool") == 0 || strcmp(str2,"bool") == 0 || strcmp(str1,"undef") == 0 || strcmp(str2,"undef") == 0 || strcmp(str1,"stringarray") == 0 || strcmp(str2,"stringarray") == 0){
 
 				//verifica operador a dar print
@@ -428,10 +427,11 @@ char * recursao_expr(is_expression_list* expr, method_var* lista_do_metodo ){
 				expr->tipo = "undef";
 				return expr->tipo;
 
-			}else if(strcmp(str1,"int")  == 0  && strcmp(str2,"double") == 0){
+
+			}else if(strcmp(str1,"int") == 0  && strcmp(str2,"double") == 0){
 				expr->tipo = "double";
 				return expr->tipo;
-			}else if(strcmp(str1,"double")  == 0 && strcmp(str2,"int") == 0){
+			}else if(strcmp(str1,"double")  == 0 && strcmp(str2,"int") == 0 ){
 				expr->tipo = "double";
 				return expr->tipo;
 			}else{
--- testes/experimento.java
+++ testes/experimento.java
@@ -1,73 +1,28 @@
-class fluxControl3 {
-    	public static int e1;
-    	
-    public static void main(String[] args) {
-		double e,e2;
-    	e1 = 1_123_2;
-    	e2 = 1.0e+12;
-    	e = e1 + (e2 = 1.0 + 2);
-    	System.out.print(e);
+class Main {
 
-    	System.out.print(" ");System.out.print("\n");
-    	System.out.print("\t\r\n\f\\\"");System.out.print("\n");
-    	System.out.print("%\t%\r%\n%\f%\\%\"%");System.out.print("\n");
-    	System.out.print("121");System.out.print("\t");
-    	System.out.print("%%");System.out.print("\f");
-    	System.out.print(" %%\t2213");System.out.print("\\");
-    	System.out.print("ª*`=)(T%D%PK!JHG.,;_`^^~%Djpcjksdojf872yyG%|\"");System.out.print("\n");
+    public static boolean b1;
+    public static void main(String[] args){
+        b1 = true;
+        System.out.print(false^!b1);
+       // if(b1&&true || b1||true && !b1^false) 
+        //if(true&&b1 || true||b1 && false^!b1) {}
+        /*double argument;
+        argument = 0.0;
+        while( Integer.parseInt(args[0]) < 10 && i < 20){
 
+            if(argument == 0){
 
-    	
-	    argument = ((Integer.parseInt(args[0]) + 5) != 10) && true;
-	    System.out.print(!!! ((!!!argument) || false) == true);
+                argument = Integer.parseInt(args[0])+1.1e-1;
 
-	    double a;
-	    a = Integer.parseInt(args[0])*Integer.parseInt(args[0])*Integer.parseInt(args[0])+Integer.parseInt(args[0])/Integer.parseInt(args[0])%Integer.parseInt(args[0]);
-	    a = Integer.parseInt(args[0]) +  1_11_23;
+            }else{
 
-	    boolean i3,i4,i5;
-	    argument = i3 = i4 = i5 = (-(1_111_32 + Integer.parseInt(args[0]))* 13_12___432) == 1;
-    	
-    	System.out.print(argument);
-
-
-    	System.out.print(a);
-
-		System.out.print(!false);
-		System.out.print("\n");
-		 a = 222_22_22;
-		double d; d = 2323_2332e1;
-		int b; b = 3;
-    			System.out.print(a);
-		//System.out.print(d%2);
-		//System.out.print(2.2%2);
-		boolean b1; b1 = true;
-		System.out.print("\na:\n");
-		System.out.print(a);
-		System.out.print("Before if\n");
-		System.out.print("After ifs\n");
-		if(d >= 2) if( d <= 2) if(2 >= d) if(2 <= d) if(d==2) if(2==d) if(d>2) if(d<2) if(2>d) if (2<d) if(d!=2) if(2!=d) {}
-		if(d >= 2.2) if( d <= 2.2) if(2.2 >= d) if(2.2 <= d) if(d==2.2) if(2.2==d) if(d>2.2) if(d<2.2) if(2.2>d) if (2.2<d) if(d!=2.2) if(2.2!=d) {}
-		if(b1==true && d >= 2.2 && 2 >= d && 2.2 <= d || 2.2== d + (d=2.3 + (a = 2) + (d = 2))) if(true==b1) if(b1!=true) if(true!=b1) {}
-		System.out.print("After ifs\n");
-		System.out.print(a * -d);
-		System.out.print("\n");
-		System.out.print("\n");
-		double d1;
-		int i; i = 0; int j; j = 0;
-		System.out.print("Before while\n");
-		while(i<10) while(10>i) while(20>j && i<10){ i=i+1;j=j+1;if(i==10) if(j==9) {}}
-		a=0;
-		if(a == 0){
-			a = a+10;
-			if(a == 10){
-				while(a < 20){
-					System.out.print(a);
-					System.out.print("\n");
-					a = a+2;
-				}
-			}
-		}
+                while( true == false){
+                    i = +1;
+                }
+                System.out.print("\\");
+            }
+            i =  +1_1_1;
+            System.out.print(argument);
+        }*/
     }
-	public static boolean argument;
 }
